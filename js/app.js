43efa02a7c5a2c4864f9873e42606cf49c76d36a
ast_vnovodid <v.novodid@astoundcommerce.com>
2021-06-17 07:13:48 +0300

    feat: add ability to distinguish sesctions during the scroll; add dynamic header creation; add 4th section to test dynamic header creation

--- js/app.js
+++ js/app.js
@@ -18,12 +18,31 @@
  * 
 */
 
+const CLASSES = {
+    ACTIVE: 'landing__active',
+    ACTIVE_SELECTOR: '.landing__active',
+    SECTION: '[data-nav]',
+    NAVIGATION_WRAPPER: '#navbar__list',
+    MENU_LINK: 'menu__link',
+    HEADER_SELECTOR: '.page__header',
+};
+
+const header = document.querySelector(CLASSES.HEADER_SELECTOR);
+const sections = document.querySelectorAll(CLASSES.SECTION);
+const navigationWrapper = document.querySelector(CLASSES.NAVIGATION_WRAPPER);
+let activeSection = document.querySelector(CLASSES.ACTIVE_SELECTOR);
+
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+const isAtTheTopOfTheViewPort = (element) => {
+    const { top } = element.getBoundingClientRect();
+    const { height: headerHeight } = header.getBoundingClientRect();
+    return window.scrollY >= ((window.scrollY + top) - headerHeight);
+};
 
 
 
@@ -34,10 +53,36 @@
 */
 
 // build the nav
+const buildNavigation = () => {
+    const fragment = document.createDocumentFragment();
+
+    sections.forEach(function (section) {
+        const { nav: text } = section.dataset;
+        const li = document.createElement('li');
+        li.textContent = text;
+        li.classList.add(CLASSES.MENU_LINK);
+        fragment.appendChild(li);
+    });
+
+    navigationWrapper.appendChild(fragment);
+};
 
 
 // Add class 'active' to section when near top of viewport
-
+const activateCurrentSection = () => {
+    for (let i = 0; i < sections.length; i++) {
+        if (sections[i] === activeSection) {
+            continue;
+        }
+        const isActive = isAtTheTopOfTheViewPort(sections[i]);
+        sections[i].classList.toggle(CLASSES.ACTIVE, isActive);
+        if (isActive) {
+            activeSection.classList.toggle(CLASSES.ACTIVE);
+            activeSection = sections[i];
+        }
+    }
+    return activeSection;
+};
 
 // Scroll to anchor ID using scrollTO event
 
@@ -48,9 +93,16 @@
  * 
 */
 
-// Build menu 
+// Build menu
+document.addEventListener('DOMContentLoaded', () => {
+    buildNavigation();
+});
 
 // Scroll to section on link click
 
+
 // Set sections as active
+window.addEventListener('scroll', () => {
+    activateCurrentSection();
+});
 
